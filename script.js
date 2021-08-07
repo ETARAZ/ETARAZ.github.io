d93df48b30331417f2a65a669d5c8bd6f06f55ca
Your Name <you@example.com>
2021-08-07 00:43:47 +0000

    change

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const list=document.querySelector(".list-navbar");
 const humb=document.querySelector(".humb");
 const closeList=document.querySelector(".close");
-
+document.documentElement.style.setProperty('--animate-duration', '0.5s');
+let i=0;
 window.addEventListener('scroll',()=>{
-    list.classList.add("none");
+    list.classList.remove("animate__slideInLeft");
+    list.classList.add("animate__slideOutLeft"); 
 });
 
 humb.addEventListener('click',()=>{
-    list.classList.toggle("none");
-})
+    if(i==0){
+        list.classList.remove("hidden");
+        i=1;
+    }
+    list.classList.remove("animate__slideOutLeft");
+    list.classList.add("animate__slideInLeft");
+
+});
 closeList.addEventListener('click',()=>{
-    list.classList.toggle("none");
+    list.classList.remove("animate__slideInLeft");
+    list.classList.add("animate__slideOutLeft");
 })
 
