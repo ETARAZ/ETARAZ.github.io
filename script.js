bb941335d4dab306fb8729617ddd2a7c183589c2
Your Name <you@example.com>
2021-08-09 23:28:43 +0000

    new commit

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ const list=document.querySelector(".list-navbar");
 const humb=document.querySelector(".humb");
 const closeList=document.querySelector(".close");
 document.documentElement.style.setProperty('--animate-duration', '0.5s');
-let i=0;
+let f=0;
 window.addEventListener('scroll',()=>{
     list.classList.remove("animate__slideInLeft");
     list.classList.add("animate__slideOutLeft"); 
 });
 
 humb.addEventListener('click',()=>{
-    if(i==0){
+    if(f==0){
         list.classList.remove("hidden");
-        i=1;
+        f=1;
     }
-    list.classList.remove("animate__slideOutLeft");
-    list.classList.add("animate__slideInLeft");
-
+        list.classList.remove("animate__slideOutLeft");
+        list.classList.add("animate__slideInLeft");
 });
 closeList.addEventListener('click',()=>{
     list.classList.remove("animate__slideInLeft");
     list.classList.add("animate__slideOutLeft");
-})
-
+});
